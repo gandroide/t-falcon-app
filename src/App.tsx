5499e1ff1166d90ca9fff5292c177c43b0cd4747
gandroide <ajontiveros82@gmail.com>
2022-05-12 12:25:40 +0100

    prettier correction

--- src/App.tsx
+++ src/App.tsx
@@ -11,42 +11,39 @@ import { FormService } from "./pages/formService";
 import { defaultTheme } from "./styles/theme";
 
 const App = () => {
-    const [isloggedIn, setisloggedIn] = useState<boolean>(true);
-    const [admin, setAdmin] = useState<boolean>(false);
+  const [isloggedIn, setisloggedIn] = useState<boolean>(true);
+  const [admin, setAdmin] = useState<boolean>(false);
 
-    // const onSelectHandler = (option: {label: string, value: string, name: string}) => {
-    //   if(option.name === 'clients'){
-    //     setSelectClient(option)
-    //   }
-    //   if(option.name === 'falcoeiros'){
-    //     setSelectFalcoeiro(option)
-    //   }
-    //   if(option.name === 'birds'){
-    //     setSelectBird(option)
-    //   }
-    //   console.log(option,"option")
-    // }
+  // const onSelectHandler = (option: {label: string, value: string, name: string}) => {
+  //   if(option.name === 'clients'){
+  //     setSelectClient(option)
+  //   }
+  //   if(option.name === 'falcoeiros'){
+  //     setSelectFalcoeiro(option)
+  //   }
+  //   if(option.name === 'birds'){
+  //     setSelectBird(option)
+  //   }
+  //   console.log(option,"option")
+  // }
 
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <GlobalStyles />
-            <h3 className="test">T-Falcon</h3>
-            <Routes>
-                {!isloggedIn && <Route path="/" element={<Login />} />}
-                {isloggedIn && !admin && (
-                    <>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/pesagem" element={<Birds />} />
-                        <Route
-                            path="/relatorio"
-                            element={<FormService />}
-                        ></Route>
-                    </>
-                )}
-                {isloggedIn && admin && <Route path="/" element={<Admin />} />}
-            </Routes>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h3 className="test">T-Falcon</h3>
+      <Routes>
+        {!isloggedIn && <Route path="/" element={<Login />} />}
+        {isloggedIn && !admin && (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/pesagem" element={<Birds />} />
+            <Route path="/relatorio" element={<FormService />}></Route>
+          </>
+        )}
+        {isloggedIn && admin && <Route path="/" element={<Admin />} />}
+      </Routes>
+    </ThemeProvider>
+  );
 };
 
 export default App;
