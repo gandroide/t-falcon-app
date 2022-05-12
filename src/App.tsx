c6dc66eaffc69d677ea4e67e64149156a6374844
gandroide <ajontiveros82@gmail.com>
2022-05-12 12:17:15 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,52 @@
-import { useState } from 'react';
-import GlobalStyles from './styles';
-import { ThemeProvider } from 'styled-components';
-import { Home } from './pages/home/Home';
+import { useState } from "react";
+import GlobalStyles from "./styles";
+import { ThemeProvider } from "styled-components";
+import { Home } from "./pages/home/Home";
 import "react-datepicker/dist/react-datepicker.css";
-import { Route, Routes } from 'react-router-dom';
-import { Login } from './pages/login';
-import { Admin } from './pages/admin';
-import { Birds } from './pages/birds/Birds';
-import { FormService } from './pages/formService';
-import { defaultTheme } from './styles/theme';
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Admin } from "./pages/admin";
+import { Birds } from "./pages/birds/Birds";
+import { FormService } from "./pages/formService";
+import { defaultTheme } from "./styles/theme";
 
 const App = () => {
-  const [isloggedIn, setisloggedIn] = useState<boolean>(true);
-  const [admin, setAdmin] = useState<boolean>(false);
+    const [isloggedIn, setisloggedIn] = useState<boolean>(true);
+    const [admin, setAdmin] = useState<boolean>(false);
 
-  // const onSelectHandler = (option: {label: string, value: string, name: string}) => {
-  //   if(option.name === 'clients'){
-  //     setSelectClient(option)
-  //   }
-  //   if(option.name === 'falcoeiros'){
-  //     setSelectFalcoeiro(option)
-  //   }
-  //   if(option.name === 'birds'){
-  //     setSelectBird(option)
-  //   }
-  //   console.log(option,"option")
-  // }
+    // const onSelectHandler = (option: {label: string, value: string, name: string}) => {
+    //   if(option.name === 'clients'){
+    //     setSelectClient(option)
+    //   }
+    //   if(option.name === 'falcoeiros'){
+    //     setSelectFalcoeiro(option)
+    //   }
+    //   if(option.name === 'birds'){
+    //     setSelectBird(option)
+    //   }
+    //   console.log(option,"option")
+    // }
 
-  return (
-    <ThemeProvider theme={defaultTheme} >
-      <GlobalStyles />
-      <h3 className="test">T-Falcon</h3>
-      <Routes>
-        {!isloggedIn && <Route path="/" element={<Login />}/>}
-        {isloggedIn && !admin && (
-        <>
-          <Route path="/" element={<Home />} />
-          <Route path="/pesagem" element={<Birds />} />
-          <Route path="/relatorio" element={<FormService />}></Route>
-        </>
-        )}
-        {isloggedIn && admin && <Route path='/' element={<Admin />}/>}
-
-      </Routes>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <GlobalStyles />
+            <h3 className="test">T-Falcon</h3>
+            <Routes>
+                {!isloggedIn && <Route path="/" element={<Login />} />}
+                {isloggedIn && !admin && (
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/pesagem" element={<Birds />} />
+                        <Route
+                            path="/relatorio"
+                            element={<FormService />}
+                        ></Route>
+                    </>
+                )}
+                {isloggedIn && admin && <Route path="/" element={<Admin />} />}
+            </Routes>
+        </ThemeProvider>
+    );
 };
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
-import { Button } from '../../components/Button';
-import { Container } from './Home.styles'
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Container } from "./Home.styles";
 
-export const Home = ({setSelectClient, selectValue}: any) => {
-  return (
-    <Container>
-      <h3>Home</h3>
-      <Button>Registrar Picagem</Button>
-      <Link to="/pesagem">Registrar Ave</Link>
-      <Link to="/relatorio">Relatorio de Serviço</Link>
-      {/* <Select selected={selectValue} onChangeHandler={setSelectClient} options={clientes}></Select> */}
-      <Link to="/">Back</Link>
-    </Container>
-  )
-}
+export const Home = ({ setSelectClient, selectValue }: any) => {
+    return (
+        <Container>
+            <h3>Home</h3>
+            <Button>Registrar Picagem</Button>
+            <Link to="/pesagem">Registrar Ave</Link>
+            <Link to="/relatorio">Relatorio de Serviço</Link>
+            {/* <Select selected={selectValue} onChangeHandler={setSelectClient} options={clientes}></Select> */}
+            <Link to="/">Back</Link>
+        </Container>
+    );
+};
