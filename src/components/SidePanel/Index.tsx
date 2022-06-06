480092ee16cadad10dfc924bde24f2d639aa844d
gandroide <ajontiveros82@gmail.com>
2022-06-06 21:38:53 +0100

    initial styles for side panel component

--- src/components/SidePanel/Index.tsx
+++ src/components/SidePanel/Index.tsx
@@ -1,7 +1,14 @@
 import { FC, useContext } from 'react';
-import { Backdrop, CloseButton, Container, Panel } from './style';
+import {
+  Backdrop,
+  ChildrenContainer,
+  CloseButton,
+  Container,
+  Panel
+} from './style';
 import { CSSTransition } from 'react-transition-group';
 import { SidepanelContext } from '../../context/Sidepanel';
+import { Button } from '../Button';
 
 interface ISidepanel {
   openPanel: boolean;
@@ -26,10 +33,14 @@ export const SidePanel: FC<ISidepanel> = ({ openPanel, children }) => {
       <Container>
         <Backdrop className="sidepanel-backdrop" onClick={handleClose} />
         <Panel className="sidepanel-panel">
-          <CloseButton className="close-button" onClick={handleClose}>
-            close
-          </CloseButton>
-          <div>{children}</div>
+          <ChildrenContainer>
+            {children}
+            <CloseButton>
+              <Button className="close-button" onClick={handleClose}>
+                back
+              </Button>
+            </CloseButton>
+          </ChildrenContainer>
         </Panel>
       </Container>
     </CSSTransition>
--- src/components/SidePanel/style.ts
+++ src/components/SidePanel/style.ts
@@ -1,42 +1,53 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Panel = styled.div`
-    display: flex;
-    width: 100%;
-    background-color: ${({theme}) => theme.palette.common.white};
-    color: ${({theme}) => theme.palette.common.black};
-    position: fixed;
-    top: 0;
-    right: 0;
-    height: 100%;
-    @media screen and (min-width: 576px) {
-        width: 50%;
-    }
-`
+  display: flex;
+  width: 100%;
+  background-color: ${({ theme }) => theme.palette.common.white};
+  color: ${({ theme }) => theme.palette.common.black};
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  @media screen and (min-width: 576px) {
+    width: 50%;
+  }
+`;
 
 export const Container = styled.div`
-    display: flex;
-    width: 100%;
-    color: white;
-    position: fixed;
-    top: 0;
-    right: 0;
-    height: 100%;
-    z-index: 1000;
-`
+  display: flex;
+  width: 100%;
+  color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  z-index: 1000;
+`;
 
 export const Backdrop = styled.div`
-    width: 100%;
-    color: white;
-    position: fixed;
-    top: 0;
-    right: 0;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    
-`
+  width: 100%;
+  color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 
-export const CloseButton = styled.button`
-    width: 4rem;
-    height: 2rem;
-`
+export const ChildrenContainer = styled.div`
+  background-color: ${({ theme }) => theme.palette.common.white};
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  padding-top: 2rem;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CloseButton = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-bottom: 10px;
+`;
