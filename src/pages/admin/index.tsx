9fb6953e752797f344f7a50a12483c8331e053c6
LuisFilipeMendonca <filipedev@outlook.pt>
2022-06-15 22:39:05 +0100

    Teste

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,8 @@
+import { Birds } from '../Birds';
 import { BirdsWeighing } from '../BirdsWeighing';
+import { UserRegistry } from '../UserRegistry';
+import { UsersRegistry } from '../UsersRegistry';
 
 export const Admin = () => {
-  return <BirdsWeighing />;
+  return <UsersRegistry />;
 };
